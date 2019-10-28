89db9c50e92341dffac506ef7a5a3f5a1923d3f8
GuruNadH_Pukkalla <53335728+GuruNadh552@users.noreply.github.com>
2019-10-28 10:59:23 +0530

    Add files via upload

---
+++ Packages/mypack/Simple123.java
@@ -0,0 +1,18 @@
+package mypack;
+public class Simple123
+{
+	public static void main(String[] args) {
+		System.out.println("Welcome!");
+	}
+	public void msg()
+	{
+		System.out.println("1 - 1");
+		System.out.println("2 - 3");
+		System.out.println("3 - 2");
+		System.out.println("4 - 1");
+		System.out.println("5 - 2");
+		System.out.println("6 - 1");
+		System.out.println("7 - 3");
+
+	}
+}
